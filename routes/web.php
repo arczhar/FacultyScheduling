6a22934e645253545241ff6bf8b39649d6a2f73f
Arczhar Mummuh <arczharjhet17@gmail.com>
2024-11-28 02:08:20 +0800

    Fixed Mange Faculty v2

--- routes/web.php
+++ routes/web.php
@@ -10,59 +10,89 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 // Admin Routes
-Route::middleware(['auth:web', 'role:Admin'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+Route::middleware(['auth:web', 'role:Admin'])->group(function () {
+    Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
 
     // Faculty Management
-    Route::resource('faculty', AdminFacultyController::class)->except(['create', 'show']);
-    Route::get('faculty/{id}/edit', [AdminFacultyController::class, 'edit'])->name('faculty.edit');
-    Route::get('faculty/{id}', [AdminFacultyController::class, 'show'])->name('faculty.show');
-    Route::put('faculty/{id}', [AdminFacultyController::class, 'update']);
+    Route::resource('/admin/faculty', AdminFacultyController::class)->names([
+        'index' => 'admin.faculty.index',
+        'store' => 'admin.faculty.store',
+        'edit' => 'admin.faculty.edit',
+        'update' => 'admin.faculty.update',
+        'destroy' => 'admin.faculty.destroy',
+    ]);
+    Route::get('/admin/faculty/{id}/edit', [AdminFacultyController::class, 'edit']);
+    Route::put('/admin/faculty/{id}', [AdminFacultyController::class, 'update']);
+    Route::get('/admin/faculty/{id}', [AdminFacultyController::class, 'show'])->name('admin.faculty.show');
 
     // Subject Management
-    Route::resource('subjects', SubjectController::class)->except(['create', 'show']);
+    Route::resource('/admin/subjects', SubjectController::class)->names([
+        'index' => 'admin.subjects.index',
+        'store' => 'admin.subjects.store',
+        'edit' => 'admin.subjects.edit',
+        'update' => 'admin.subjects.update',
+        'destroy' => 'admin.subjects.destroy',
+    ]);
 
     // Room Management
-    Route::resource('rooms', RoomController::class)->except(['create', 'show']);
+    Route::resource('/admin/rooms', RoomController::class)->names([
+        'index' => 'admin.rooms.index',
+        'store' => 'admin.rooms.store',
+        'update' => 'admin.rooms.update',
+        'destroy' => 'admin.rooms.destroy',
+    ]);
 
     // Schedule Management
-    Route::resource('schedules', ScheduleController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
-    Route::post('schedules', [ScheduleController::class, 'store'])->name('schedules.store');
-    Route::delete('schedules/{id}', [ScheduleController::class, 'destroy'])->name('schedules.destroy');
+    Route::resource('/admin/schedules', ScheduleController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy'])->names([
+        'index' => 'admin.schedules.index',
+        'create' => 'admin.schedules.create',
+        'store' => 'admin.schedules.store',
+        'edit' => 'admin.schedules.edit',
+        'update' => 'admin.schedules.update',
+        'destroy' => 'admin.schedules.destroy',
+    ]);
+    Route::delete('/admin/schedules/{id}', [ScheduleController::class, 'destroy'])->name('admin.schedules.destroy');
 });
 
 // Program Chair Routes
-Route::middleware(['auth:web', 'role:Program Chair'])->prefix('program-chair')->name('programchair.')->group(function () {
-    Route::get('/dashboard', [AdminController::class, 'programChairDashboard'])->name('dashboard');
+Route::middleware(['auth:web', 'role:Program Chair'])->group(function () {
+    Route::get('/program-chair/dashboard', [AdminController::class, 'programChairDashboard'])->name('programchair.dashboard');
 
-    // Schedule Management (limited for Program Chair)
-    Route::resource('schedules', ScheduleController::class)->only(['index', 'create', 'store', 'edit', 'update']);
-    Route::delete('schedules/{id}', [ScheduleController::class, 'destroy'])->name('schedules.destroy');
+    // Program Chair Schedule Management
+    Route::resource('/program-chair/schedules', ScheduleController::class)->only(['index', 'create', 'store', 'edit', 'update'])->names([
+        'index' => 'programchair.schedules.index',
+        'create' => 'programchair.schedules.create',
+        'store' => 'programchair.schedules.store',
+        'edit' => 'programchair.schedules.edit',
+        'update' => 'programchair.schedules.update',
+    ]);
+    Route::delete('/program-chair/schedules/{id}', [ScheduleController::class, 'destroy'])->name('programchair.schedules.destroy');
 });
 
 // Faculty Routes
-Route::middleware(['auth:faculty'])->prefix('faculty')->name('faculty.')->group(function () {
-    Route::get('/dashboard', [FacultyController::class, 'dashboard'])->name('dashboard');
-    Route::get('/schedule', [FacultyController::class, 'viewSchedule'])->name('schedule');
-    Route::get('/profile', [FacultyController::class, 'viewProfile'])->name('profile');
-    Route::post('/profile/update', [FacultyController::class, 'updateProfile'])->name('profile.update');
-    Route::get('/change-password', [FacultyController::class, 'showChangePasswordForm'])->name('change-password');
-    Route::post('/change-password', [FacultyController::class, 'updatePassword'])->name('update-password');
+Route::middleware(['auth:faculty'])->group(function () {
+    Route::get('/faculty/dashboard', [FacultyController::class, 'dashboard'])->name('faculty.dashboard');
+    Route::get('/faculty/schedule', [FacultyController::class, 'viewSchedule'])->name('faculty.schedule');
+    Route::get('/faculty/profile', [FacultyController::class, 'viewProfile'])->name('faculty.profile');
+    Route::post('/faculty/profile/update', [FacultyController::class, 'updateProfile'])->name('faculty.profile.update');
+    Route::get('/faculty/change-password', [FacultyController::class, 'showChangePasswordForm'])->name('faculty.change-password');
+    Route::post('/faculty/change-password', [FacultyController::class, 'updatePassword'])->name('faculty.update-password');
 });
 
 // AJAX Routes
-Route::prefix('ajax')->name('ajax.')->group(function () {
-    Route::get('get-faculty-details/{facultyId}', [ScheduleController::class, 'getFacultyDetails'])->name('get-faculty-details');
-    Route::get('get-subjects', [ScheduleController::class, 'getSubjects'])->name('get-subjects');
-    Route::get('get-subject-details/{id}', [SubjectController::class, 'getSubjectDetails'])->name('get-subject-details');
-    Route::post('check-schedule-conflict', [ScheduleController::class, 'checkAndSaveSchedule'])->name('check-schedule-conflict');
+Route::get('/get-faculty-details/{facultyId}', [ScheduleController::class, 'getFacultyDetails'])->name('get-faculty-details');
+Route::get('/get-subjects', [ScheduleController::class, 'getSubjects'])->name('get-subjects');
+Route::get('/get-subject-details/{id}', [SubjectController::class, 'getSubjectDetails'])->name('get-subject-details');
+Route::post('/check-schedule-conflict', [ScheduleController::class, 'checkAndSaveSchedule'])->name('check-schedule-conflict');
+Route::post('/admin/schedules', [ScheduleController::class, 'store'])->name('admin.schedules.store');
+
+// Miscellaneous Routes
+Route::get('/simple-page', function () {
+    return view('simple');
 });
-
-// Miscellaneous Testing Routes
-Route::view('/simple-page', 'simple')->name('simple-page');
-Route::view('/test-modal', 'test-modal')->name('test-modal');
-
-// Debug Route
+Route::get('/test-modal', function () {
+    return view('test-modal');
+})->name('test-modal');
 Route::get('/debug-role', function () {
     return Auth::check() ? 'Logged in as ' . Auth::user()->role : 'Not logged in';
 })->middleware('auth:web');
